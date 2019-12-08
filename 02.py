f6f21600e60ff562343159b7d3819e88be3fc089
Brian Malehorn <bmalehorn@gmail.com>
2019-12-07 16:05:35 -0800

    better bootstrap.py

---
+++ 02.py
@@ -0,0 +1,11 @@
+import math
+from aoc import parse, flatten, pp
+import aoc
+
+
+def main():
+    numbers = parse("02-easy.txt")
+    pp(numbers)
+
+
+main()
--- bootstrap.py
+++ bootstrap.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+
+import sys
+
 SKELETON = """
 import math
 from aoc import parse, flatten, pp
@@ -5,18 +9,23 @@ import aoc
 
 
 def main():
-    numbers = parse("01-1.txt")
-    print(numbers)
+    numbers = parse("%s-easy.txt")
+    pp(numbers)
 
 
 main()
-
 """
 
 
 def main():
-    
-    pass
+    assert len(sys.argv) == 2
+    number_str = "%02d" % int(sys.argv[1])
+    with open("%s.txt" % number_str, "w"):
+        pass
+    with open("%s-easy.txt" % number_str, "w"):
+        pass
+    with open("%s.py" % number_str, "w") as f:
+        f.write((SKELETON % number_str).strip())
 
 
 main()
